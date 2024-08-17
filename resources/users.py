50ef5870fb8a2a9e8d3f1092acd825a9e52aecdc
Gurucharan R <thisischaran@gmail.com>
2024-08-18 00:07:19 +0530

    Register function implamented

--- resources/users.py
+++ resources/users.py
@@ -11,18 +11,16 @@ from Schema import userloginschema, userloginresponseschema, userregisterschema,
 
 blp = Blueprint("users",__name__,description ="useer operations")
 
-@blp.route('/login')
+@blp.route('/register')
 class userlogin(MethodView):
     @blp.arguments(userloginschema)
     @blp.response(201,userloginresponseschema)
     def post(self,user_data):
         reguser = dbusertable(**user_data)
-
         try:
-            print("i have come here")
+
             db.session.add(reguser)
             db.session.commit()
-            print("Step 2i have come here")
 
         except IntegrityError:
             abort(400, message="user id already available")
